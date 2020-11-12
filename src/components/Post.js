9f5c2885286a38fb89490e580275b3137ab5c200
benoit barges <benoitbarges@gmail.com>
2020-11-12 10:40:33 +0100

    add proptypes to Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Post({ author, title, created, comments, url, setUser}) {
 
@@ -19,4 +20,13 @@ export default function Post({ author, title, created, comments, url, setUser}) 
       </div>
     </div>
   )
+}
+
+Post.propTypes = {
+  author: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  created: PropTypes.number.isRequired,
+  comments: PropTypes.number.isRequired,
+  url: PropTypes.string.isRequired,
+  setUser: PropTypes.func.isRequired
 }
